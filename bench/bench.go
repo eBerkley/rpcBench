a1be238134b0491daba5aaa68ca95bd58a44582d
Ethan Berkley <ethanberkley@ku.edu>
2024-05-16 13:03:17 -0500

    added some bench utils

---
+++ bench/bench.go
@@ -0,0 +1,35 @@
+package bench
+
+import (
+	"context"
+	"testing"
+)
+
+type Server any
+
+type Benchable func(context.Context, Server) error
+
+func (b Benchable) call(ctx context.Context, s Server) error {
+	return b(ctx, s)
+}
+
+func RunSubBenchmarks(name string, b *testing.B, bnch Benchable, ctx context.Context, s Server) {
+	b.Run(name+" Concurrent", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			err := bnch(ctx, s)
+			if err != nil {
+				b.Error(err.Error())
+			}
+		}
+	})
+	b.Run("Geo Nearby Parallel", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				err := bnch(ctx, s)
+				if err != nil {
+					b.Error(err.Error())
+				}
+			}
+		})
+	})
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/eBerkley/rpcBench
+
+go 1.21.6

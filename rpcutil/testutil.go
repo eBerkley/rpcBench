8f00215d8bfb9897a0bbb1a547717093f14956a7
Ethan Berkley <ethanberkley@ku.edu>
2024-05-21 11:52:34 -0500

    I figured out how generics work I think

---
+++ rpcutil/testutil.go
@@ -0,0 +1,40 @@
+package rpcutil
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path"
+	"runtime"
+	"strings"
+)
+
+var _, filename, _, _ = runtime.Caller(0)
+var output_path = path.Join(path.Dir(filename), "../../hotel_reservation/util/output.csv")
+
+var Output = flag.String("output", output_path, "Location to put output CSV")
+
+func formatOutput(r rune) rune {
+	if r == ',' {
+		return ' '
+	}
+	if r == '{' || r == '}' {
+		return -1
+	}
+	return r
+}
+
+func WriteTestResults(service string, input string, output string) {
+
+	output_csv, err := os.OpenFile(*Output, os.O_WRONLY|os.O_CREATE, 0666)
+
+	if err != nil {
+		fmt.Printf("%f\n", err)
+		os.Exit(1)
+	}
+
+	defer output_csv.Close()
+
+	output_csv.Write([]byte(service + "," + input + "," + strings.Map(formatOutput, output) + "\n"))
+
+}
---
+++ rpcutil/util.go
@@ -0,0 +1,58 @@
+package rpcutil
+
+import (
+	"context"
+	"testing"
+)
+
+type Server any
+
+const NUM_TESTS = 5
+
+type Request interface {
+	Make()
+	Get() string
+}
+
+type Result interface {
+	Get() string
+}
+
+func NewRequest[T Request]() T {
+	var t T
+	t.Make()
+	return t
+}
+
+type RPCFunc func(context.Context, Server) (Request, Result, error)
+
+func (rpc RPCFunc) Test(ctx context.Context, s Server, t *testing.T, testName string) {
+	for i := 0; i < NUM_TESTS; i++ {
+		req, res, err := rpc(ctx, s)
+		if err != nil {
+			t.Error(err.Error())
+		}
+		WriteTestResults(testName, req.Get(), res.Get())
+	}
+}
+
+func (rpc RPCFunc) Bench(ctx context.Context, s Server, b *testing.B, benchName string) {
+	b.Run(benchName+" Concurrent", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, _, err := rpc(ctx, s)
+			if err != nil {
+				b.Error(err.Error())
+			}
+		}
+	})
+	b.Run(benchName+" Parallel", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				_, _, err := rpc(ctx, s)
+				if err != nil {
+					b.Error(err.Error())
+				}
+			}
+		})
+	})
+}
